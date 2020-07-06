be54a2475661cc29437c51ec49cae96582c5e809
Gary Cordero <GaryCordero1690@gmail.com>
2020-07-06 17:10:05 -0400

    Project index and create action added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Project.delete_all 
+Member.delete_all
 
 Project.create(name:"Cubicle",deadline:"Jan 1st, 2021")
 Project.create(name:"Project Ava",deadline:"Jun 23, 2021")
+
+
+15.times do 
+  Member.create(name:Faker::Name.unique.name,image_url:Faker::Avatar.unique.image,role:Faker::Job.title)
+end
