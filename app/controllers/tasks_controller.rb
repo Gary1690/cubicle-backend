dabc3cfd2650dceb3a877939be3b7ddfccd7e2e1
Gary Cordero <GaryCordero1690@gmail.com>
2020-07-07 19:44:52 -0400

    creating task and assigments

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+
+  def create 
+    task = Task.create(task_params)
+    members = Member.find(params[:memberIds])
+    task.members << members
+    render json: task, include: [:members]
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name,:priority,:project_id)
+  end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ApplicationRecord
   belongs_to :project
   has_many :assigments
   has_many :members, through: :assigments
+  accepts_nested_attributes_for :members
+
+  def member_attributes=(member_attributes)
+    member_attributes.each do |member_attribute|
+      member = Member.find_by(member_attribute)
+      self.members << member
+    end
+  end
 end
