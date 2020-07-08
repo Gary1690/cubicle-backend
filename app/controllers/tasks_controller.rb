c4c8d5e69ea8e8c047c32cd159cc8cd6f0f515ff
Gary Cordero <GaryCordero1690@gmail.com>
2020-07-08 16:03:10 -0400

    deleting a task completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,9 +16,17 @@ class TasksController < ApplicationController
     render json: task, include: [:members]
   end
 
+  def destroy
+    task = Task.find_by(id:params[:id])
+    task.assigments.each{|assignment|assignment.destroy}
+    task.destroy
+    render json: task
+  end
+
   private
   def task_params
     params.require(:task).permit(:name,:priority,:project_id)
   end
 
+  
 end
